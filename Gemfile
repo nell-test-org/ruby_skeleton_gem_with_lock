b6cb07809aeb968dcea71f3dd96a3e5f1511c212
Nell Shamrell <nellshamrell@gmail.com>
2019-10-04 15:52:43 -0700

    initial commit
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in ruby_skeleton_gem_with_lock.gemspec
+gemspec
---
+++ lib/ruby_skeleton_gem_with_lock.rb
@@ -0,0 +1,6 @@
+require "ruby_skeleton_gem_with_lock/version"
+
+module RubySkeletonGemWithLock
+  class Error < StandardError; end
+  # Your code goes here...
+end
